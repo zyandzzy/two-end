10c4418b6e050049830daf1bade2998f0580264c
ZZYandZY <18827561151@163.com>
2025-12-03 16:59:59 +0800

    初始项目创建

---
+++ myproject-backend/src/main/java/com/example/MyprojectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyprojectBackendApplication.class, args);
+    }
+
+}
